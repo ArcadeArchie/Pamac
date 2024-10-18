2e5486436033646b666c1ec6830152226ccfff5c
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2024-10-18 13:46:32 +0200

    further map out config struct

--- src/Pamac.Core/Config/SignatureVerificationLevel.cs
+++ src/Pamac.Core/Config/SignatureVerificationLevel.cs
@@ -15,12 +15,25 @@ public struct SignatureVerificationLevel
 public enum VerificationLevel
 {
     Never,
-    Optional,
-    Required
+    Required,
+    Optional
 }
 
 public enum TrustLevel
 {
     TrustedOnly,
     TrustAll
+}
+//https://gitlab.archlinux.org/pacman/pacman/-/blob/master/src/pacman/conf.c?ref_type=heads#L491
+internal enum ALPM_SIGLEVEL
+{
+    ALPM_SIG_PACKAGE = (1 << 0),
+    ALPM_SIG_PACKAGE_OPTIONAL = (1 << 1),
+    ALPM_SIG_PACKAGE_MARGINAL_OK = (1 << 2),
+    ALPM_SIG_PACKAGE_UNKNOWN_OK = (1 << 3),
+    ALPM_SIG_DATABASE = (1 << 10),
+    ALPM_SIG_DATABASE_OPTIONAL = (1 << 11),
+    ALPM_SIG_DATABASE_MARGINAL_OK = (1 << 12),
+    ALPM_SIG_DATABASE_UNKNOWN_OK = (1 << 13),
+    ALPM_SIG_USE_DEFAULT = (1 << 30),
 }
